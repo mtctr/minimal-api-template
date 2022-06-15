f871d1f7f64530981dee36d05de9440a6ffd55d2
Mateus <mateustargino95@gmail.com>
2022-06-15 11:55:05 -0300

    code upload

---
+++ MinimalApiTemplate/Modules/OrdersModule.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalApiTemplate.Modules
+{
+    public class OrdersModule : IModule
+    {
+        public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGet("/orders", () => "Orders endpoint!");
+            return endpoints;
+        }
+
+        public IServiceCollection RegisterModule(IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
